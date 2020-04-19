da4f598c8de20713599e86c40413d90075b9e607
Vladislav <vladrommel4@gmail.com>
2020-04-20 00:56:58 +0400

    Initial Commit

---
+++ RPS v.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS v.3
+//
+//  Created by User on 20.04.2020.
+//  Copyright © 2020 Vladislav Rommel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
